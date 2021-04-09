8ee81679905405d8c8d16a0beff5a0e2c288384f
Arthur-C-Newton <arthurn889@gmail.com>
2021-04-09 13:33:50 +0100

    Import training samples from single shapefile

--- script.py
+++ script.py
@@ -4,7 +4,7 @@
 # import the required modules
 from zipfile import ZipFile
 import os
-from os import path
+from pathlib import Path
 import rasterio
 from rasterio import mask
 from rasterio.windows import get_data_window
@@ -12,14 +12,19 @@ import geopandas as gpd
 import gdal
 from rsgislib.segmentation import segutils
 import rsgislib.rastergis
+import rsgislib.rastergis.ratutils
 
-extent_path = "input\\extent.shp"  # replace later to allow users to set this when running script
+# file paths and other elements that should be set by user
+extent_path = "input\\extent.shp"  # replace these paths later to allow users to set this when running script
 tmp_path = ".\\tmp"
-stack_path = "tmp\\stack.tif"
+stack_path = "tmp\\stack.tif"  # users should only want to change this if they want to only save the stacked image
+shp_path = "input\\training_data.shp"
 
 for file in os.listdir(".\\input"):
     if file.endswith(".zip"):
         zip_path = os.path.join(".\\input", file)
+
+class_col_name = 'Ecological'  # this is the name of the column of the training data that contains class strings
 
 # create ist of files that are band images
 archive = ZipFile(zip_path, 'r')
@@ -41,7 +46,7 @@ meta = band2.meta
 meta.update(count=len(bands), driver="GTiff")  # update the metadata to allow multiple bands
 
 # clip to extent if one is provided
-if path.exists(extent_path):
+if os.path.exists(extent_path):
     extent = gpd.read_file(extent_path)
     shapes = extent["geometry"]
     clipped_image, clipped_transform = mask.mask(band2, shapes, crop=True)
@@ -57,7 +62,7 @@ if path.exists(extent_path):
                 out_image, transform = mask.mask(src1, shapes, crop=True)
                 dst.write_band(id, out_image[0])
                 print("Writing band...")
-else:
+else:  # if no extent is provided, the full image is staved to disk (very slow)
     # create a single stacked geotiff based on the image metadata
     with rasterio.open(stack_path, 'w', **meta) as dst:
         for id, layer in enumerate(bands, start=1):
@@ -67,30 +72,42 @@ else:
 print("Multi-band GeoTiff saved successfully at tmp/stack.tif!")
 
 # re-import data and save as KEA
-# raster = gdal.Open(stack_path)
-# raster = gdal.Translate("tmp\\raster.kea", raster, format="KEA")
+raster = gdal.Open(stack_path)
+raster = gdal.Translate("tmp\\raster.kea", raster, format="KEA")
 
-# in_img = "tmp\\raster.kea"
-# clumps = "clumps_image.kea"
+in_img = "tmp\\raster.kea"
+clumps = "clumps_image.kea"
 
 
 # segment the image using rsgislib
-# segutils.runShepherdSegmentation(in_img, clumps, tmpath=tmp_path, numClusters=100, minPxls=100, distThres=100, sampling=100, kmMaxIter=200)
-# band_info = []
-# band_info.append(rsgislib.rastergis.BandAttStats(band=1, minField='BlueMin', maxField='BlueMax', meanField='BlueMean', stdDevField='BlueStdev'))
-# band_info.append(rsgislib.rastergis.BandAttStats(band=2, minField='GreenMin', maxField='GreenMax', meanField='GreenMean', stdDevField='GreenStdev'))
-# band_info.append(rsgislib.rastergis.BandAttStats(band=3, minField='RedMin', maxField='RedMax', meanField='RedMean', stdDevField='RedStdev'))
-# band_info.append(rsgislib.rastergis.BandAttStats(band=4, minField='NIRMin', maxField='NIRMax', meanField='NIRMean', stdDevField='NIRStdev'))
-# rsgislib.rastergis.populateRATWithStats(in_img, clumps, band_info)
+segutils.runShepherdSegmentation(in_img, clumps, tmpath=tmp_path, numClusters=100, minPxls=100, distThres=100, sampling=100, kmMaxIter=200)
+band_info = []
+band_info.append(rsgislib.rastergis.BandAttStats(band=1, minField='BlueMin', maxField='BlueMax', meanField='BlueMean', stdDevField='BlueStdev'))
+band_info.append(rsgislib.rastergis.BandAttStats(band=2, minField='GreenMin', maxField='GreenMax', meanField='GreenMean', stdDevField='GreenStdev'))
+band_info.append(rsgislib.rastergis.BandAttStats(band=3, minField='RedMin', maxField='RedMax', meanField='RedMean', stdDevField='RedStdev'))
+band_info.append(rsgislib.rastergis.BandAttStats(band=4, minField='NIRMin', maxField='NIRMax', meanField='NIRMean', stdDevField='NIRStdev'))
+rsgislib.rastergis.populateRATWithStats(in_img, clumps, band_info)
 
 
-# import training data
-shp_path = "input\\training_data.shp"
-
+# import training data from shapefile
 training_data = gpd.read_file(shp_path)
-print(training_data.head())
+print(training_data.head())  # for testing
 
-class_col_name = 'Ecological'
+# save each land cover class as a separate shapefile
 for l_class, training_class in training_data.groupby(class_col_name):
     training_class.to_file(tmp_path + "\\" + l_class + ".shp")
 
+# read shapefiles of each class into dictionary
+class_dict = dict()
+index = 0
+for file in os.listdir(tmp_path):
+    if file.endswith(".shp"):
+        class_path = os.path.join(tmp_path, file)
+        class_filename = Path(class_path).stem
+        class_name_nospace = class_filename.replace(" ", "_")
+        index += 1
+        class_dict[class_name_nospace] = [index, class_path]
+
+class_int_col_in = "ClassInt"
+class_name_col = "ClassStr"
+rsgislib.rastergis.ratutils.populateClumpsWithClassTraining(clumps, class_dict, tmp_path, class_int_col_in, class_name_col)
